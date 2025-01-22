59cef70d0e46be4e7e8237be405cc9b8fb0716ef
Slav <kurochkinsalv@gmail.com>
2025-01-21 22:17:36 -0500

    Add unit tests for AppBar component and enhance coverage configuration

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,6 +3,18 @@ module.exports = {
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
   },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
   moduleFileExtensions: ['js', 'jsx'],
   testEnvironment: 'node',
+  collectCoverage: true, // Enable coverage collection
+  coverageDirectory: 'coverage', // Directory to output coverage reports
+  coverageReporters: ['html', 'text'], // Generate HTML and text coverage reports
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}', // Include all JS and JSX files in src
+    '!src/**/*.test.{js,jsx}', // Exclude test files
+    '!src/index.js', // Exclude entry point if not needed
+    '!src/**/styles.js', // Exclude specific files like styles
+  ],
 };
